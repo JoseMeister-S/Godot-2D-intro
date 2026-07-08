c11859d0314bf7c50dd77848caafe001dffdedbf
Jose Manuel Sanchez <99229719+JoseMeister-S@users.noreply.github.com>
2026-07-08 18:39:42 +0200

    Working hud plus score

--- main.gd
+++ main.gd
@@ -4,8 +4,7 @@ extends Node
 var score
 
 func _ready() -> void:
-	new_game()
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,13 +14,16 @@ func _process(delta: float) -> void:
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
-	print("entra 1")
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
-	print("entra 2")
 	$StartTimer.start()
+	get_tree().call_group("mobs", "queue_free")
+	
 
 
 func _on_mob_timer_timeout() -> void:
@@ -53,6 +55,7 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
